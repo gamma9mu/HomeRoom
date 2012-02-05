abb0ea15830b4cbd75d0bf1a755b929487bd6176
Brian <brian@MOLLY>
2012-02-04 22:46:37 -0600

    Add other Dublin Core attributes.

--- RDFGenerator/RDFMetaData.cs
+++ RDFGenerator/RDFMetaData.cs
@@ -9,15 +9,29 @@ namespace RDFGenerator
 {
     public class RDFMetaData
     {
-        public String Title { get; set; } // - Title
+        public String Title { get; set; } // - A name
+        public String Creator { get; set; } // - Primary "maker"
+        public String Subject { get; set; } // - Topic
         public String Description { get; set; } // - A brief description
-        public String Creator { get; set; } // Who wrote it
-        //public String copyright_owner { get; set; } // 
-        //public String copyright_year { get; set; } // 
-        public String Format { get; set; } // - MIME type
-        public String Type { get; set; } // - Genre
-        public Uri Location { get; set; } // - For RDF:about
-        public long Size { get; set; } // File size in bytes
+        public String Publisher { get; set; } // - what it says
+
+        // TODO should this be a list?
+        public String Contributor { get; set; } // - what it says
+
+        public DateTime Date { get; set; } // - A relavent date
+
+        public String Type { get; set; } // - Genre?
+        public String Format { get; set; } // - Format, eg. MIME type
+
+        public Uri Identifier { get; set; } // - Reference (used in rdf:RDF>rdf:about)
+        public String Language { get; set; } // - maybe a 2-char international code
+
+        // TODO is this useful?
+        public String Relation { get; set; } // - Related item
+        public String Coverage { get; set; } // - Applicability
+        public String Rights { get; set; } // - IP info
+
+        public long Size { get; set; } // - File size in bytes
 
         public void Write(String filename)
         {
@@ -40,12 +54,20 @@ namespace RDFGenerator
             var root = new XElement(rdfns + "Description",
                 new XAttribute(XNamespace.Xmlns + "rdf", rdfns.NamespaceName),
                 new XAttribute(XNamespace.Xmlns + "dc", dcns.NamespaceName),
-                new XAttribute(rdfns + "about", Location.ToString()),
+                new XAttribute(rdfns + "about", Identifier.ToString()),
                 new XElement(dcns + "Title", Title),
                 new XElement(dcns + "Description", Description),
                 new XElement(dcns + "Format", Format),
                 new XElement(dcns + "Creator", Creator),
-                new XElement(dcns + "Size", Size.ToString(CultureInfo.InvariantCulture)),
+                new XElement(dcns + "Language", Language),
+                new XElement(dcns + "Relation", Relation),
+                new XElement(dcns + "Coverage", Coverage),
+                new XElement(dcns + "Subject", Subject),
+                new XElement(dcns + "Publisher", Publisher),
+                new XElement(dcns + "Contributer", Contributor),
+                new XElement(dcns + "Rights", Rights),
+                new XElement(dcns + "Date", Date.ToShortDateString()),
+                new XElement("Size", Size.ToString(CultureInfo.InvariantCulture)),
                 new XElement(dcns + "Type", Type)
             );
             IEnumerable<XElement> empty = from el in root.Elements()
