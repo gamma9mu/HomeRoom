1571a2af2b26e90ec488d997e8c125b36e43827e
Brian <brian@MOLLY>
2012-03-14 17:10:01 -0500

    Add request class to pass to the controller.

--- StudentInformation.cs
+++ StudentInformation.cs
@@ -13,4 +13,18 @@
             tactilePercent = tp;
         }
     }
+
+    public class Request
+    {
+        public int connectionSpeed { get; private set; }
+        public string topic { get; private set; }
+        public StudentInformation studentInformation { get; private set; }
+
+        public Request(StudentInformation student, string topic, int connectionSpeed)
+        {
+            this.studentInformation = student;
+            this.topic = topic;
+            this.connectionSpeed = connectionSpeed;
+        }
+    }
 }
