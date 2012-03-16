2d2e67db8e4392b9d099540d9898fae44bfefbd4
Brian <brian@MOLLY>
2012-03-16 17:13:59 -0500

    Split writing to RdfFile to combine Description tags.

--- RDFMetaData.cs
+++ RDFMetaData.cs
@@ -29,61 +29,40 @@ namespace HomeRoom
         public String Rights { get; set; } //  IP info
         public long Size { get; set; } // File size in bytes
 
-        public void Write(String filename)
-        {
-            FileStream file = File.Open(filename, FileMode.CreateNew, FileAccess.Write);
-            Write(file);
-        }
-
-        public void Write(FileStream file)
-        {
-            if (!file.CanWrite)
-            {
-                throw new ArgumentException(
-                    rdf.RDFMetaData_Write_File_does_not_have_write_permissions_, "file");
-            }
-            var sout = new StreamWriter(file);
-            sout.Write(ToXml());
-        }
-
-        public String ToXml()
+        public XElement getDescription()
         {
             XNamespace rdfns = rdf.RDF_NS;
             XNamespace dcns = rdf.DC_NS;
             XNamespace mdns = rdf.MD_NS;
-            var root = new XElement(rdfns + "RDF",
-                new XAttribute(XNamespace.Xmlns + "rdf", rdfns.NamespaceName),
-                new XAttribute(XNamespace.Xmlns + "dc", dcns.NamespaceName),
-                new XAttribute(XNamespace.Xmlns + "md", mdns),
-                new XElement(rdfns + "Description",
-                    new XAttribute(rdfns + "about", Identifier.ToString()),
-                    new XElement(dcns + "Title", Title),
-                    new XElement(dcns + "Description", Description),
-                    new XElement(dcns + "Format", Format),
-                    new XElement(dcns + "Creator", Creator),
-                    new XElement(dcns + "Language", Language),
-                    new XElement(dcns + "Relation", Relation),
-                    new XElement(dcns + "Coverage", Coverage),
-                    new XElement(dcns + "Subject", Subject),
-                    new XElement(dcns + "Publisher", Publisher),
-                    new XElement(dcns + "Contributer", Contributor),
-                    new XElement(dcns + "Rights", Rights),
-                    new XElement(dcns + "Date", Date.ToShortDateString()),
-                    new XElement(mdns + "Size", Size.ToString(CultureInfo.InvariantCulture)),
-                    new XElement(dcns + "Type", Type)
-                )
+            var root = new XElement(
+                rdfns + "Description",
+                new XAttribute(rdfns + "about", Identifier.ToString()),
+                new XElement(dcns + "Title", Title),
+                new XElement(dcns + "Description", Description),
+                new XElement(dcns + "Format", Format),
+                new XElement(dcns + "Creator", Creator),
+                new XElement(dcns + "Language", Language),
+                new XElement(dcns + "Relation", Relation),
+                new XElement(dcns + "Coverage", Coverage),
+                new XElement(dcns + "Subject", Subject),
+                new XElement(dcns + "Publisher", Publisher),
+                new XElement(dcns + "Contributer", Contributor),
+                new XElement(dcns + "Rights", Rights),
+                new XElement(dcns + "Date", Date.ToShortDateString()),
+                new XElement(mdns + "Size", Size.ToString(CultureInfo.InvariantCulture)),
+                new XElement(dcns + "Type", Type)
             );
 
             // Remove empty and Size==0 tags.
             root.Descendants().Where(x => x.IsEmpty).Remove();
             root.Descendants(mdns + "Size").Where(x => x.Value == "0").Remove();
             
-            return root.ToString();
+            return root;
         }
 
         public override string ToString()
         {
-            return ToXml();
+            return getDescription().ToString();
         }
     }
 }
---
+++ RdfFile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.IO;
+
+namespace HomeRoom
+{
+    public class RdfFile
+    {
+        private XElement rdfRoot;
+
+        public RdfFile()
+        {
+            XNamespace rdfns = rdf.RDF_NS;
+            XNamespace dcns = rdf.DC_NS;
+            XNamespace mdns = rdf.MD_NS;
+            rdfRoot = new XElement(rdfns + "RDF",
+                new XAttribute(XNamespace.Xmlns + "rdf", rdfns.NamespaceName),
+                new XAttribute(XNamespace.Xmlns + "dc", dcns.NamespaceName),
+                new XAttribute(XNamespace.Xmlns + "md", mdns));
+        }
+
+        public void addDescription(XElement rdfDescription)
+        {
+            rdfRoot.Add(rdfDescription);
+        }
+
+        public void Save(StreamWriter stream)
+        {
+            rdfRoot.Save(stream);
+        }
+    }
+}
