1a856a4f2c3e3e0398aa536c49cd2e94f257a52a
Brian <brian@MOLLY>
2012-03-16 03:09:38 -0500

    Change namespace to HomeRoom.

--- RDFMetaData.cs
+++ RDFMetaData.cs
@@ -5,7 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Xml.Linq;
 
-namespace RDFGenerator
+namespace HomeRoom
 {
     public class RDFMetaData
     {
--- rdf.Designer.cs
+++ rdf.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace RDFGenerator {
+namespace HomeRoom {
     using System;
     
     
