a02c6d748c6cd62c6d32f0134f0229b889f6349f
Brian <brian@MOLLY>
2012-03-15 23:58:19 -0500

    Move appid and search URL to property files.

--- Crawl.cs
+++ Crawl.cs
@@ -4,55 +4,54 @@ using System.Net;
 using System.Text;
 using System.Xml;
 
-public class Crawl
+namespace Crawler
 {
-    /// AppId given by Bing;
-    private static sealed string AppId = "79468C520824589EBDAAFEBF29C53AFD6D0CF891";
-    /// The generic url for querying
-    private static sealed string searchUrl = "http://api.bing.net/xml.aspx?Appid={0}&sources={1}&query={2}";
-
-    /// Holds urls returned by query
-    static string[] urls = new string[50];
-
-    public void find(string query, string sources = "web")
+    public class Crawl
     {
-        string completeUri = String.Format(searchUrl, AppId, sources, query);
-        HttpWebRequest webRequest = (HttpWebRequest) WebRequest.Create(completeUri);
-        HttpWebResponse webResponse = (HttpWebResponse) webRequest.GetResponse();
-        XmlReader reader = XmlReader.Create(webResponse.GetResponseStream());
+        /// Holds urls returned by query
+        static string[] urls = new string[50];
 
-        int i = 0;
-        while (i < 10)
+        public void find(string query, string sources = "web")
         {
-            reader.ReadToFollowing("web:Title");
-            string title = reader.ReadString();
-            if (title == null) break;
-            
-            reader.ReadToFollowing("web:Description");
-            string description = reader.ReadString();
-            
-            reader.ReadToFollowing("web:DateTime");
-            string datetime = reader.ReadString();
+            string completeUri = String.Format(Properties.Resources.SearchUrl,
+                Properties.Resources.AppId, sources, query);
+            HttpWebRequest webRequest = (HttpWebRequest) WebRequest.Create(completeUri);
+            HttpWebResponse webResponse = (HttpWebResponse) webRequest.GetResponse();
+            XmlReader reader = XmlReader.Create(webResponse.GetResponseStream());
 
-            reader.ReadToFollowing("web:Url");
-            string url = reader.ReadString();
+            int i = 0;
+            while (i < 10)
+            {
+                reader.ReadToFollowing("web:Title");
+                string title = reader.ReadString();
+                if (title == null) break;
 
-            Console.Out.WriteLine("web:Title: " + title);
-            Console.Out.WriteLine("web:Description: " + description);
-            Console.Out.WriteLine("web:DateTime: " + datetime);
-            Console.Out.WriteLine(url + "\n");
-            
-            urls[i] = url;
-            i++;
+                reader.ReadToFollowing("web:Description");
+                string description = reader.ReadString();
+
+                reader.ReadToFollowing("web:DateTime");
+                string datetime = reader.ReadString();
+
+                reader.ReadToFollowing("web:Url");
+                string url = reader.ReadString();
+
+                Console.Out.WriteLine("web:Title: " + title);
+                Console.Out.WriteLine("web:Description: " + description);
+                Console.Out.WriteLine("web:DateTime: " + datetime);
+                Console.Out.WriteLine(url + "\n");
+
+                urls[i] = url;
+                i++;
+            }
+
+            /// To be implemented...
+            /// Distribute url results to another program to be ranked
         }
 
-        /// To be implemented...
-        /// Distribute url results to another program to be ranked
-    }
-
-    static void Main(string[] args)
-    {
-        Crawl c = new Crawl();
-        c.find("cool+stuff");
+        static void Main(string[] args)
+        {
+            Crawl c = new Crawl();
+            c.find("cool+stuff");
+        }
     }
 }
