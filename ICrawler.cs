ed767cb4a3f194e3a0eab3d54a4305d74bd08348
Brian <brian@MOLLY>
2012-03-26 14:24:44 -0500

    Create crawler interface.

---
+++ ICrawler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HomeRoom
+{
+    /// <summary>
+    /// Provide a common interface for different content crawlers.
+    /// </summary>
+    interface ICrawler
+    {
+        public List<Result> find(string query);
+    }
+}
