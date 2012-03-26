d3ccd3a6eb464c475c1b8017b2c769783c4bbb09
Brian <brian@MOLLY>
2012-03-26 15:41:47 -0500

    Create a search factory interface and a Bing implementation.

---
+++ Bing.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HomeRoom
+{
+    /// <summary>
+    /// Factory class for Bing searches.
+    /// </summary>
+    public static class Bing : ISearchFactory
+    {
+        /// <summary>
+        /// Get a BingWebCrawler.
+        /// </summary>
+        /// <returns>A fresh BingWebCrawler</returns>
+        public static ICrawler createWebCrawler()
+        {
+            return new BingWebCrawler();
+        }
+
+        /// <summary>
+        /// Get a BingImageCrawler.
+        /// </summary>
+        /// <returns>A fresh BingImageCrawler</returns>
+        public static ICrawler createImageCrawler()
+        {
+            return new BingImageCrawler();
+        }
+
+        public static ICrawler createAudioCrawler()
+        {
+            throw new NotImplementedException("Coming soon...");
+        }
+
+        public static ICrawler createVideoCrawler()
+        {
+            throw new NotImplementedException("Coming soon...");
+        }
+    }
+}
---
+++ ISearchFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HomeRoom
+{
+    /// <summary>
+    /// Common interface for search factories.
+    /// </summary>
+    public interface ISearchFactory
+    {
+        static ICrawler createWebCrawler();
+        static ICrawler createImageCrawler();
+        static ICrawler createAudioCrawler();
+        static ICrawler createVideoCrawler();
+    }
+}
