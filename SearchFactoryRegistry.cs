12c908a1911faa83ccadc01c63015212ea76f9cb
Brian <brian@MOLLY>
2012-03-31 22:10:34 -0500

    Create SearchFactoryRegistry to load all search providers.

---
+++ SearchFactoryRegistry.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HomeRoom
+{
+    public class SearchFactoryRegistry
+    {
+        private List<ISearchFactory> factories =
+            new List<ISearchFactory>();
+
+        private static SearchFactoryRegistry singleton = null;
+
+        /// <summary>
+        /// The SearchFactoryRegistry constructor will take a StringCollection
+        /// from the project app.config and load the classes.
+        /// </summary>
+        private SearchFactoryRegistry()
+        {
+            System.Collections.Specialized.StringCollection factoryNames =
+                Properties.Settings.Default.SearchFactories;
+
+            foreach (var factoryName in factoryNames){
+                ISearchFactory factory = (ISearchFactory)
+                    Type.GetType(factoryName).GetConstructor(new Type[]{})
+                    .Invoke(null);
+                if (factory != null)
+                    factories.Add(factory);
+            }
+        }
+
+        /// <summary>
+        /// Get the one SearchFactoryRegistry object.
+        /// </summary>
+        /// <returns>A SearchFactoryRegistry.</returns>
+        public static SearchFactoryRegistry getInstance()
+        {
+            if (singleton == null) singleton = new SearchFactoryRegistry();
+            return singleton;
+        }
+
+        /// <summary>
+        /// Get an ISearchFactory by name.
+        /// </summary>
+        /// <param name="name">The name of the ISEarchFactory.</param>
+        /// <returns>The requested factory or null if it was not found.</returns>
+        public static ISearchFactory getFactory(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Get an array of all the known ISearchFactory objects
+        /// </summary>
+        /// <returns>An ISearchFactory[] of all known search factories.</returns>
+        public ISearchFactory[] getAllFactories()
+        {
+            return factories.ToArray();
+        }
+    }
+}
