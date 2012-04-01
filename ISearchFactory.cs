987db7b12658ae97e7f437a901b3fba0ae8508b9
Brian <brian@MOLLY>
2012-04-01 18:03:30 -0500

    Doc: ISearchFactory

--- ISearchFactory.cs
+++ ISearchFactory.cs
@@ -10,9 +10,32 @@ namespace HomeRoom
     /// </summary>
     public interface ISearchFactory
     {
+        /// <summary>
+        /// Provide a web page crawler.
+        /// </summary>
+        /// <returns>An <code>ICrawler</code> specialized to retrieve web page
+        /// items.</returns>
         ICrawler createWebCrawler();
+
+        /// <summary>
+        /// Provide a image crawler.
+        /// </summary>
+        /// <returns>An <code>ICrawler</code> specialized to retrieve image
+        /// items.</returns>
         ICrawler createImageCrawler();
+
+        /// <summary>
+        /// Provide a audio crawler.
+        /// </summary>
+        /// <returns>An <code>ICrawler</code> specialized to retrieve audio
+        /// items.</returns>
         ICrawler createAudioCrawler();
+
+        /// <summary>
+        /// Provide a video crawler.
+        /// </summary>
+        /// <returns>An <code>ICrawler</code> specialized to retrieve video
+        /// items.</returns>
         ICrawler createVideoCrawler();
     }
 }
