5dc9fe0d2d2c26f7234eb02bfb923d04604aea3f
Brian <brian@MOLLY>
2012-04-01 17:59:48 -0500

    Doc: Result

--- Result.cs
+++ Result.cs
@@ -3,21 +3,60 @@
 namespace HomeRoom
 {
     /// <summary>
-    /// A storage class for passing Bing search results.
+    /// A storage class for returning search results.
     /// </summary>
     public class Result
     {
+        /// <summary>
+        /// The URL of the resource.
+        /// </summary>
         public Uri Url { get; private set; }
 
+        /// <summary>
+        /// The title of the resource.
+        /// </summary>
         public string Title { get; set; }
+
+        /// <summary>
+        /// Any available description of the resource.
+        /// </summary>
         public string Description { get; set; }
+
+        /// <summary>
+        /// The most-relevant date of the item.
+        /// </summary>
         public DateTime Datetime { get; set; }
+
+        /// <summary>
+        /// For multimedia items, the height.
+        /// </summary>
         public int Height { get; set; }
+
+        /// <summary>
+        /// For multimedia items, the width.
+        /// </summary>
         public int Width { get; set; }
+
+        /// <summary>
+        /// The file size of the item.
+        /// </summary>
         public int Size { get; set; }
+
+        /// <summary>
+        /// For multimedia items, the duration in seconds.
+        /// </summary>
         public int Length { get; set; }
+
+        /// <summary>
+        /// The MIME type of the item.
+        /// </summary>
         public string MimeType { get; set; }
 
+        /// <summary>
+        /// Construct a new Result.
+        /// </summary>
+        /// <param name="url">The URL of the resource. This cannot be
+        /// <code>null</code>.</param>
         public Result(string url)
         {
             if (url == null)
