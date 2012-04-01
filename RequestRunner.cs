e257b716c76444e4b4f7872011cb96b124a8f430
Brian <brian@MOLLY>
2012-03-31 19:25:41 -0500

    Restructure RequestRunner for easier future changes.

--- RequestRunner.cs
+++ RequestRunner.cs
@@ -16,37 +16,66 @@ namespace HomeRoom
         
         private Request request;
 
+        /// <summary>
+        /// Create a new request service object.
+        /// </summary>
+        /// <param name="request">The request to service.</param>
         public RequestRunner(Request request)
         {
             System.Diagnostics.Debug.Assert(request != null);
             this.request = request;
         }
 
+        /// <summary>
+        /// Use any available search providers to crawl for items.
+        /// </summary>
+        /// <returns>A list of any items that were found.</returns>
         public List<Result> retrieveItems()
         {
             preFilter();
+            int expectedResultCount = webCount + imageCount + videoCount + audioCount;
+
             Bing bing = new Bing();
-            ICrawler c = bing.createWebCrawler();
+            
+            List<Result> results = new List<Result>(expectedResultCount);
+            results.AddRange(searchWith(bing));
+
+            return results;
+        }
+
+        /// <summary>
+        /// Searches with a given ISearchFactory's crawlers.
+        /// </summary>
+        /// <param name="searchfactory">The search factory to use.</param>
+        /// <returns>The combined results of the searches.</returns>
+        private List<Result> searchWith(ISearchFactory searchfactory)
+        {
+            ICrawler c = searchfactory.createWebCrawler();
             List<Result> results = c.find(request.topic, webCount);
 
-            c = bing.createImageCrawler();
+            c = searchfactory.createImageCrawler();
             results.AddRange(c.find(request.topic, imageCount));
 
-            c = bing.createVideoCrawler();
+            c = searchfactory.createVideoCrawler();
             results.AddRange(c.find(request.topic, videoCount));
 
-            c = bing.createAudioCrawler();
+            c = searchfactory.createAudioCrawler();
             results.AddRange(c.find(request.topic, audioCount));
 
             return results;
         }
 
+        /// <summary>
+        /// Weight the amount of each type of file to retrieve based on the
+        /// user's learning style and connection speed.
+        /// </summary>
         private void preFilter()
         {
-            webCount = request.studentInformation.visualPercent * RETRIEVAL_FACTOR;
-            imageCount = request.studentInformation.visualPercent * RETRIEVAL_FACTOR;
-            audioCount = request.studentInformation.auralPercent * RETRIEVAL_FACTOR;
-            videoCount = request.studentInformation.visualPercent * RETRIEVAL_FACTOR;
+            StudentInformation si = request.studentInformation;
+            webCount = si.visualPercent * RETRIEVAL_FACTOR;
+            imageCount = si.visualPercent * RETRIEVAL_FACTOR;
+            audioCount = si.auralPercent * RETRIEVAL_FACTOR;
+            videoCount = si.visualPercent * RETRIEVAL_FACTOR;
             
             // scale video count by connection speed
             double speedFactor = request.connectionSpeed / 1500000;
