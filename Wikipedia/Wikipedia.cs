7404f5f72b4ac12f9c8f7297d99468b530d050c9
Brian <brian@MOLLY>
2012-04-05 16:08:56 -0500

    Add Wikipedia search provider. [NO COMPILE]
    Adds a class and modifies the config to add Wikipedia searches.

---
+++ Wikipedia/Wikipedia.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HomeRoom
+{
+    /// <summary>
+    /// Factory class for Wikipedia searches.
+    /// </summary>
+    class Wikipedia : ISearchFactory
+    {
+        /// <summary>
+        /// Get a WikipediaWebCrawler.
+        /// </summary>
+        /// <returns>A fresh WikipediaWebCrawler</returns>
+        public ICrawler createWebCrawler()
+        {
+            return new WikipediaWebCrawler();
+        }
+
+        public ICrawler createImageCrawler()
+        {
+            return new NoopCrawler();
+        }
+
+        public ICrawler createAudioCrawler()
+        {
+            return new NoopCrawler();
+        }
+
+        public ICrawler createVideoCrawler()
+        {
+            return new NoopCrawler();
+        }
+    }
+}
